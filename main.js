653fffeae27d72adbf311df683b1d96f2f7117e2
lpolishchuk <iryna.polishchuk.l@gmail.com>
2018-09-16 20:28:14 +0300

    Add method for changing background-color of the box. Changed box border color from 'green' to 'lightgrey'.

--- main.js
+++ main.js
@@ -6,38 +6,45 @@ console.log(mainContainer);
 
 let boxHistory = [];
 
-
-BoxConstructor(30,30);
-console.log(mainContainer);
-
-
 let changeColor = function (rowNumber, boxNumber) {
 	console.log(rowNumber, boxNumber);
+	let choosenBox = document.getElementById(`row${rowNumber}box${boxNumber}`);
+	choosenBox.setAttribute("class", "paintedBox");
 }
 
-let BoxConstructor = function (x, y) {
+BoxConstructor = function (x, y) {
 
-	for (var index = 1; index <= y; index++) {
+	for (var index = 0; index < y; index++) {
 		let rowElement = document.createElement("div");
 		rowElement.setAttribute("class", "rowElement");
 		rowElement.setAttribute("id", `rowElement${index}`);
 		mainContainer.appendChild(rowElement);
+		boxHistory.push([index]);
 
 		// boxHistory.push([index]);
 		//rows append
-		for (var i = 1; i <= x; i++) {
+		for (var i = 0; i < x; i++) {
 			let box = document.createElement("div");
 			box.setAttribute("class", "emptyBox");
-			box.setAttribute("id", `box${i}`);
+			box.setAttribute("id", `row${index}box${i}`);
 			let rowNumber = index, boxNumber = i;
 			box.onclick = function () {
 				changeColor(rowNumber, boxNumber);
 			};
 			rowElement = document.getElementById(`rowElement${index}`);
 			rowElement.appendChild(box);
+			boxHistory[index].push([i])
+			boxHistory[index][i].currentstate = false;
+			boxHistory[index][i].previousState = false;
 			// boxHistory[index-1][index].push(i);
 			//box append
 		}
 	}
 }
+
+
+BoxConstructor(30,30);
+// console.log(mainContainer);
+
+// console.log(boxHistory);
 
